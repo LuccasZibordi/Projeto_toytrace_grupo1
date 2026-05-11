7237eb1826d3549c68419fae3446addc2b115d98
Luccas Gomes Zibordi <luccaszibordi@gmail.com>
2026-05-11 14:08:04 -0300

    Semana 3 resolvendo o conflito da branch JV

--- src/trace_runtime.c
+++ src/trace_runtime.c
@@ -104,24 +104,24 @@ static int resume_until_next_syscall(pid_t child, int signal_to_deliver)
 
 static int wait_for_syscall_stop(pid_t child, int *status)
 {
-    /*
-     * TODO Semana 3:
-     *
-     * Espere o filho com waitpid().
-     *
-     * Retorne:
-     *   1 se a parada foi uma parada de syscall;
-     *   0 se o filho terminou normalmente ou por sinal;
-     *  -1 em erro.
-     *
-     * Dicas:
-     * - WIFEXITED e WIFSIGNALED indicam fim do processo.
-     * - WIFSTOPPED indica que o processo parou.
-     * - com PTRACE_O_TRACESYSGOOD, syscall-stops aparecem com bit 0x80.
-     * - paradas SIGTRAP comuns nao devem ser entregues de volta ao filho.
-     */
-    fprintf(stderr, "erro: TODO Semana 3: implementar wait_for_syscall_stop()\n");
-    return -1;
+if (waitpid(child, status,0)<0)
+    {
+        perror("erro");
+        return -1;
+    }
+
+    if(WIFEXITED(*status)) || WIFSIGNALED(*status)
+    {
+        return 0;
+    }
+
+
+    if(wifstopped(*status)== (sigtrap | 0x80))
+    {
+        return 1;
+    }
+
+    return 0;
 }
 
 int trace_program(char *const argv[],
