f8ff34a10b8ee6d2d000f854c30ff8938ac6ddf8
Gengo250 <miguelgengo28@gmail.com>
2026-05-14 21:59:33 -0300

    Semana 4 | student_debug_raw_event [concluido]

--- src/student/formatter.c
+++ src/student/formatter.c
@@ -27,8 +27,16 @@ void student_debug_raw_event(const struct syscall_event *ev, char *buf,
    * A pergunta importante da Semana 4 e:
    * por que a mesma syscall aparece duas vezes?
    */
-  snprintf(buf, bufsz, "pid=%d %s %s", ev->pid, syscall_name(ev->syscall_no),
-           ev->entering ? "entrada" : "saida");
+  if (ev->entering) {
+    snprintf(
+        buf, bufsz,
+        "pid=%d syscall=%s entrada argumentos(%#lx, %#lx, %#lx, %#lx, %#lx)",
+        ev->pid, syscall_name(ev->syscall_no), ev->args[0], ev->args[1],
+        ev->args[2], ev->args[3], ev->args[4]);
+  } else {
+    snprintf(buf, bufsz, "pid=%d syscall=%s saida retorno=%ld", ev->pid,
+             syscall_name(ev->syscall_no), ev->ret);
+  }
 }
 
 void student_format_event(const struct syscall_event *ev, char *buf,
